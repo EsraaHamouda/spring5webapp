cf696ce27ae20b4d0294b0da57b86b566fc76e2a
EsraaHamouda <esraahamouda94@gmail.com>
2020-08-13 20:17:45 +0200

    view h2 memory db

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,35 +2,44 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+
 @Component
 public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Book book1 = new Book("title: Sort you life out","123", new HashSet<>());
+        Book book1 = new Book("title: Sort you life out", "123", new HashSet<>());
         Author author1 = new Author("Robert", "John", new HashSet<>());
+        Publisher publisher = new Publisher("Cairo", "Street 1", "Gareden City", "1234", new HashSet<>());
+
         book1.getAuthors().add(author1);
         author1.getBooks().add(book1);
 
         authorRepository.save(author1);
         bookRepository.save(book1);
+        publisherRepository.save(publisher);
 
-        Book book2 = new Book("title: cinderella","456", new HashSet<>());
+
+        Book book2 = new Book("title: cinderella", "456", new HashSet<>());
         Author author2 = new Author("William", "shakespeare", new HashSet<>());
         book2.getAuthors().add(author2);
         author2.getBooks().add(book2);
@@ -38,9 +47,16 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(author2);
         bookRepository.save(book2);
 
+        publisher.getBooks().add(book1);
+        book1.setPublisher(publisher);
+        publisher.getBooks().add(book2);
+        book2.setPublisher(publisher);
         System.out.println("Started in bootstrap");
-        System.out.printf("Number of books: %d", bookRepository.count());
-        System.out.printf("Number of authors: %d", authorRepository.count());
+
+        System.out.printf("Number of publisher: %d \n", publisherRepository.count());
+        System.out.printf("Number of books: %d \n", publisher.getBooks().size());
+        System.out.printf("Number of books: %d\n", bookRepository.count());
+        System.out.printf("Number of authors: %d\n", authorRepository.count());
 
 
     }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,10 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -18,14 +16,19 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books;
+
     public Publisher() {
     }
 
-    public Publisher(String city, String address, String state, String zip) {
+    public Publisher(String city, String address, String state, String zip, Set<Book> books) {
         this.city = city;
         this.address = address;
         this.state = state;
         this.zip = zip;
+        this.books = books;
     }
 
     public Long getId() {
@@ -66,6 +69,14 @@ public class Publisher {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
